6686b6c9c46e6d5b3c173da537914a36f68866bf
karthik-kumar-g <g24ai1004@iitj.ac.in>
2025-07-05 18:45:55 +0530

    Update regression.py

--- regression.py
+++ regression.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0,os.path.dirname(os.path.abspath(__file__)))
 
 from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.linear_model import LinearRegression
